2681eba943f2617e3ef9f506049b35c6dba24572
JH <kk_ong2233@naver.com>
2023-11-09 09:57:57 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 132 ms, Memory: 14296 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,24 @@
+import java.io.*; // Scanner(java.util 클래스)와 달리 BufferReader는 java.io 클래스를 import한다.
+
+public class Main {
+    public static void main(String args[]) throws IOException { // BufferReader는 IOExeption의 예외처리가 필수이다.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine()); // 입력받은 데이터가 줄마다 String으로 반환되므로 int 형변환 처리.
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i + j <= n) {
+                    bw.write(" ");
+                } else {
+                    bw.write("*");
+                }
+
+            }
+            bw.write("\n");
+        }
+
+        bw.flush(); // Buffer Flush
+    }
+}
