fe261a5a550ce93dbd51f8de756a4c80397a16d3
김정현 <kk_ong2233@naver.com>
2024-03-22 19:12:31 +0900

    [Bronze I] Title: 약수, Time: 148 ms, Memory: 15880 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    int n = Integer.parseInt(br.readLine());
+    int min = Integer.MAX_VALUE;
+    int max = Integer.MIN_VALUE;
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    for(int i = 0; i < n; i++){
+      int num = Integer.parseInt(st.nextToken());
+
+      if (num > max) {
+        max = num;
+      }
+
+      if (num < min){
+        min = num;
+      }
+    }
+
+
+    bw.write(min * max + "\n");
+    bw.flush();
+    bw.close();
+  }
+}
