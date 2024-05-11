30a6dd588d075a296cc59b7fc856d5626b7c89d6
김정현 <kk_ong2233@naver.com>
2024-05-11 18:51:24 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 140 ms, Memory: 14380 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.java
@@ -0,0 +1,45 @@
+import java.io.*;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(br.readLine());
+
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= i; j++) {
+        bw.write("*");
+      }
+
+      for (int j = 1; j <= 2 * (n - i); j++) {
+        bw.write(" ");
+      }
+
+      for (int j = 1; j <= i; j++) {
+        bw.write("*");
+      }
+
+      bw.write("\n");
+    }
+
+    for (int i = n - 1; i > 0; i--) {
+      for (int j = 1; j <= i; j++) {
+        bw.write("*");
+      }
+
+      for (int j = 1; j <= 2 * (n - i); j++) {
+        bw.write(" ");
+      }
+
+      for (int j = 1; j <= i; j++) {
+        bw.write("*");
+      }
+
+      bw.write("\n");
+    }
+
+    bw.flush();
+    bw.close();
+  }
+}
