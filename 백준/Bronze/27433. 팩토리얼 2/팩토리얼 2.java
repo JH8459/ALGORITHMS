270f7235b28ef158b320b756c25dec9ec4de9d8a
김정현 <kk_ong2233@naver.com>
2024-08-07 08:42:54 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 112 ms, Memory: 15964 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(br.readLine());
+
+    bw.write(getFactorial(n) + "\n");
+    bw.flush();
+    bw.close();
+  }
+
+  public static long getFactorial(int n){
+    if(n <= 1){
+      return 1;
+    }
+
+    return n * getFactorial(n - 1);
+  }
+}
