7b57e15a84f0fb3e4a39324db12630e1f99a6361
김정현 <kk_ong2233@naver.com>
2024-09-17 22:28:15 +0900

    [Bronze V] Title: 새로운 시작, Time: 96 ms, Memory: 13980 KB -BaekjoonHub

---
+++ 백준/Bronze/15962. 새로운 시작/새로운 시작.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    bw.write("파이팅!!\n");
+    bw.flush();
+    bw.close();
+  }
+}
