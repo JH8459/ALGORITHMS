d05b56f3177f9b4e034d57ff0c8bd9ff0a52e660
김정현 <kk_ong2233@naver.com>
2024-09-29 23:41:38 +0900

    [Bronze V] Title: 이상한 기호, Time: 116 ms, Memory: 15888 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15964. 이상한 기호/이상한 기호.java
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -7,8 +7,8 @@ public class Main {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     StringTokenizer st = new StringTokenizer(br.readLine());
-    int a = Integer.parseInt(st.nextToken());
-    int b = Integer.parseInt(st.nextToken());
+    long a = Long.parseLong(st.nextToken());
+    long b = Long.parseLong(st.nextToken());
 
     bw.write((a + b) * (a - b) + "\n");
     bw.flush();
