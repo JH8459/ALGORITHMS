8b6ba6fa607eeba3d349b9f57d53fd238d100e84
김정현 <kk_ong2233@naver.com>
2024-10-07 23:07:13 +0900

    [level 0] Title: 두 수의 연산값 비교하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.js
+++ 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.js
@@ -2,5 +2,5 @@ function solution(a, b) {
     const ab = a.toString() + b.toString();
     const ab2 = 2 * a * b;
     
-    return +ab >= + ab2 ? +ab : +ab2;
+    return +ab >= +ab2 ? +ab : +ab2;
 }
