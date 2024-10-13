0f0eac065cf3dbd535499193e0ea871b589ca62e
김정현 <kk_ong2233@naver.com>
2024-10-14 08:22:39 +0900

    [level 0] Title: 카운트 업, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.js
@@ -0,0 +1,10 @@
+function solution(start_num, end_num) {
+    const answer = [];
+    
+    while (start_num <= end_num){
+        answer.push(start_num);
+        start_num++;
+    }
+    
+    return answer;
+}
