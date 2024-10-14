8fb8f59fb69b73477f88882651bde68d16532aeb
김정현 <kk_ong2233@naver.com>
2024-10-14 21:23:15 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -0,0 +1,15 @@
+function solution(n) {
+    const answer = [n];
+    
+    while(n > 1){
+        if(n % 2){
+            n = 3 * n + 1;
+        } else {
+            n = n / 2;    
+        } 
+        
+        answer.push(n);
+    }
+    
+    return answer;
+}
