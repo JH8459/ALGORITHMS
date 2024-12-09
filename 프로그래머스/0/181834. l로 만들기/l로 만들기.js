a43af45447aff03e287288d46c4d6e4ea5e8809b
김정현 <kk_ong2233@naver.com>
2024-12-09 11:20:02 +0900

    [level 0] Title: l로 만들기, Time: 11.60 ms, Memory: 38.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.js
@@ -0,0 +1,9 @@
+function solution(myString) {
+    return [...myString].map((char) => {
+        if(char.charCodeAt() < 108){
+            return 'l';
+        } else {
+            return char;
+        }
+    }).join('');
+}
