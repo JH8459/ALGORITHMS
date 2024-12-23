fa04a209468efc2111118496a464cd91e8033d5d
김정현 <kk_ong2233@naver.com>
2024-12-23 10:11:28 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 53.52 ms, Memory: 65.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -0,0 +1,17 @@
+function solution(participant, completion) {
+    const map = new Map();
+
+    participant.forEach(name => {
+        map.set(name, (map.get(name) || 0) + 1);
+    });
+
+    completion.forEach(name => {
+        map.set(name, map.get(name) - 1);
+    });
+
+    for (const [name, count] of map) {
+        if (count > 0) {
+            return name;
+        }
+    }
+}
