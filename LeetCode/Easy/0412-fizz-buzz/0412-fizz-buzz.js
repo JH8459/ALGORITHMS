0e6be209d73c1d986acccd5dc0869fd7eb87447d
김정현 <kk_ong2233@naver.com>
2025-03-10 16:40:55 +0900

    [Easy] Title: 412-fizz-buzz, Time: 1 ms (63.49%), Space: 57.3 MB (23.85%) - LeetHub

---
+++ LeetCode/Easy/0412-fizz-buzz/0412-fizz-buzz.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function(n) {
+    return Array.from({ length: n }, (_, i) => {
+        const num = i + 1;
+        let result = '';
+
+        if (num % 3 === 0) result += 'Fizz';
+        if (num % 5 === 0) result += 'Buzz';
+
+        return result || num.toString();
+    });
+};
