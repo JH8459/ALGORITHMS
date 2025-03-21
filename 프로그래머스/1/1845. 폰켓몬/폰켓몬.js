fc04c902f8ff20c5b02c3ccda70241d91bcf2559
김정현 <kk_ong2233@naver.com>
2025-03-21 09:43:06 +0900

    [level 1] Title: 폰켓몬, Time: 0.59 ms, Memory: 34.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,8 +1,4 @@
 function solution(nums) {
-    const maxSelect = nums.length / 2;
-    const uniqueNums = new Map();
-    
-    nums.forEach((num) => uniqueNums.set(num ,true));
-    
-    return Math.min(uniqueNums.size, maxSelect);
+    // 중복을 허용하지 않는 Set 자료형 사용, num / 2 길이와 비교하여 리턴한다.
+    return Math.min(new Set(nums).size, nums.length / 2);
 }
