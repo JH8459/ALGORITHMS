eb30303709cf3f60e997378bee16d24f945d9c8f
김정현 <kk_ong2233@naver.com>
2025-03-21 14:05:07 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 40.69 ms, Memory: 66.7 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,17 +1,18 @@
 function solution(participant, completion) {
-    const map = new Map();
+    const participantMap = new Map();
 
-    participant.forEach(name => {
-        map.set(name, (map.get(name) || 0) + 1);
-    });
+  // 참가자 카운트 증가
+  for (const name of participant) {
+    participantMap.set(name, (participantMap.get(name) || 0) + 1);
+  }
 
-    completion.forEach(name => {
-        map.set(name, map.get(name) - 1);
-    });
+  // 완주자 카운트 감소
+  for (const name of completion) {
+    participantMap.set(name, participantMap.get(name) - 1);
+  }
 
-    for (const [name, count] of map) {
-        if (count > 0) {
-            return name;
-        }
-    }
+  // 카운트가 1로 남은 사람이 완주하지 못한 사람
+  for (const [name, count] of participantMap.entries()) {
+    if (count > 0) return name;
+  }
 }
